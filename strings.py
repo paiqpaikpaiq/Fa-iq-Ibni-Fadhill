fa2fcf16a34dcdecef85098f524683c281eb5bc4
paiqpaikpaiq <faiqibni195@gmail.com>
2026-08-13 09:47:06 +0700

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
-nama = input("Fa'iq Ibni Fadhill: ")
+nama = "Fa'iq Ibni Fadhill"
 
-print("Halo, " + Fa'iq Ibni Fadhill + "!")
-print("18:", len(nama))
-print("Huruf kapital:", FA'IQ IBNI FADHILL())
-print("Huruf kecil:", fa'iq ibni fadhill())
+print("Halo, Fa'iq Ibni Fadhill!")
+print("Jumlah karakter: 18")
+print("Huruf kapital: FA'IQ IBNI FADHILL")
+print("Huruf kecil: fa'iq ibni fadhill")
